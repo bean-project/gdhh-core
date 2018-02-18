776959de54e7411c25d12c1292267e1047acc45b
Peter Pierre <binh@sunrise.vn>
2018-02-18 18:38:25 +0700

    Change Component Structure

--- Entity/User/Base/AppUser.php
+++ Entity/User/Base/AppUser.php
@@ -4,9 +4,10 @@ namespace App\Entity\User\Base;
 use App\Entity\HoSo\ThanhVien;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as AbstractedUser;
 
 /** @ORM\MappedSuperclass */
-class AppUser {
+class AppUser extends AbstractedUser{
 	
 	const ROLE_ADMIN = 'ROLE_ADMIN';
 	const ROLE_HUYNH_TRUONG = 'ROLE_HUYNH_TRUONG';
